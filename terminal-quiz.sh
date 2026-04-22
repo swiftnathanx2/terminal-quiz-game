11c5339ca0b7976b189d41d8d6564c079f3c0a4b
Valery <Nyoh Jonathan Smith>
2026-04-22 07:01:35 +0100

    Feat: add practice mode, high score system and polished overall game flow

--- terminal-quiz.sh
+++ terminal-quiz.sh
@@ -46,7 +46,7 @@ load_questions() {
 
         while [[  "$user_answer" != "A" &&  "$user_answer" != "B" && "$user_answer" != "C" && "$user_answer" != "D" ]]; do
             echo "Invalid response!: Try again"
-            read -p "Enter your answer (A/B/C/D): " user_answer
+            read -p  "Enter your answer (A/B/C/D): " user_answer
             user_answer=${user_answer^^}
 
         done
@@ -107,9 +107,64 @@ load_questions() {
         fi
 
     #prompting the user to continue thereby pausing the game for the user to see their response at the bottom of the screen
-        read -p $'\nPress enter to continue.....'
+        read -r -p $'\nPress enter to continue.....'
 
             
     done
 
 }
+
+#conditional logic to load the high score system
+
+if [[ "$mode" = "highscores" ]]; then
+    echo -e "==============HIGH SCORE SYSTEM==============\n"
+    printf "%-6s %10s %7s %11s\n" "Player" "  Percentage  " "Score " "Date"
+    awk -F'|' '{print $0}' highscores.txt  | sort -n -rt"|" -k2,2 | head -n 5 | awk -F'|' '{printf "%-6s %10s %10s %15s\n", $1, $2, $3,$4}'
+    echo "================================================"
+    exit 0
+elif [[ "$mode" = "practice" ]]; then
+    echo -e "\n==============Welcome to Quiz Master Practice Mode===============\n"
+    echo -e "\n---------------Practice till you are good at it\n"
+    read -p "Enter your name" user_name
+     load_questions
+    exit 0
+fi
+    
+
+#guard check: checking if the file exists and is not empty
+    if [[ ! -f "$file" ]]; then
+        echo "File is missing or does not exist"
+        
+    elif [[ ! -s "$file" ]]; then
+            echo "File is empty"
+    else
+        echo "File exists and is not empty"
+
+    fi
+clear
+#Username and welcome intro to game
+echo "==============================Welcome to Quiz Master==========================="
+echo -e "\n-------------Test your knowledge on tech, geography and more!!--------------\n"
+read -p "Enter your name: " user_name
+echo "Welcome $user_name! Have fun!" 
+read -p "Press enter to launch game!"
+echo -e "\nLOADING GAME===========================================\n"
+sleep 0.5
+
+
+
+load_questions 
+#final score calculation
+score=$((CORRECT * 100 /QUESTION_COUNT))
+
+#High score section
+echo "$user_name|$score|$CORRECT/$QUESTION_COUNT|$(date +%Y-%m-%d)" >> highscores.txt
+
+
+#Game summary section
+
+echo "==========GAME SUMMARY========"
+echo "---------------------------------------"
+echo -e "Correct Questions: $CORRECT\nIncorrect Questions: $INCORRECT\nLongest Streak: $LONGEST_STREAK\nFinal score: $score%\n"
+echo "----------------------------------------"
+echo "================================"
